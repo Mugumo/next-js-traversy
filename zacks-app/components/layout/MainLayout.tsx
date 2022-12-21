6f6e3e96b454abb1be9b7bc7853254ff834627ce
zackgakuo <zackgakuo@gmail.com>
2022-12-21 08:20:38 +0300

    fix css import to main layout

--- zacks-app/components/layout/MainLayout.tsx
+++ zacks-app/components/layout/MainLayout.tsx
@@ -1,11 +1,14 @@
 import { readFileSync } from 'fs'
 import React from 'react'
-import styles from '../../styles/LAyout.module.css'
+import styles from '../../styles/Layout.module.css'
 
 const MainLayout = ({children}) => {
     return (
         <div className={styles.container}>
             <main className={styles.main}>
+                <h1>
+                    Hello there, folks!
+                </h1>
                 {children}
             </main>
         </div>
